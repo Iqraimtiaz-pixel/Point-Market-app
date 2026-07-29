44847aac242ffe35444e87b2ba4d78a94b11bb45
Iqraimtiaz-pixel <iqraimtiazqazi.4455@gmail.com>
2026-07-29 16:31:06 +0500

    Update main.jsx to display a test message
    Replaced App component with a styled div displaying 'POINT MARKET TEST'.

--- SRC/main.jsx
+++ SRC/main.jsx
@@ -1,5 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "40px",
+        color: "red",
+        fontWeight: "bold",
+        fontFamily: "Arial"
+      }}
+    >
+      POINT MARKET TEST
+    </div>
   </React.StrictMode>
 );
