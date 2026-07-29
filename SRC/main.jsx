04c0fa5f3f46ee98e3a208e5ba23ac1ad6ed4e99
Iqraimtiaz-pixel <iqraimtiazqazi.4455@gmail.com>
2026-07-29 16:37:59 +0500

    Replace test div with App component in main.jsx

--- SRC/main.jsx
+++ SRC/main.jsx
@@ -1,21 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        fontSize: "40px",
-        color: "red",
-        fontWeight: "bold",
-        fontFamily: "Arial"
-      }}
-    >
-      POINT MARKET TEST
-    </div>
+    <App />
   </React.StrictMode>
 );
